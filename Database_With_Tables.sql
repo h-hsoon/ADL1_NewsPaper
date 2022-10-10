61b25f550fc219380d931fde8db57d4332b8d029
MaximusDD2002 <2maxim2@mail.ru>
2022-10-10 22:13:33 +0200

    Implemented new tables
    Implemented new tables for the database

--- Database_With_Tables.sql
+++ Database_With_Tables.sql
@@ -4,6 +4,7 @@ CREATE or REPLACE TABLE `ADL1_NewsPaper`.`users`
 (`user_id` INT NOT NULL AUTO_INCREMENT ,
 `user_name` VARCHAR(50) NOT NULL ,
 `password` VARCHAR(100) NOT NULL ,
+`image` VARCHAR(255),
 PRIMARY KEY (`user_id`),
 UNIQUE (`user_name`))
 ENGINE = InnoDB;
@@ -11,6 +12,7 @@ ENGINE = InnoDB;
 CREATE or REPLACE TABLE `ADL1_NewsPaper`.`news`
 (`news_id` INT NOT NULL AUTO_INCREMENT ,
 `title` VARCHAR(50) NOT NULL ,
+`category` TEXT NOT NULL ,
 `content` TEXT NOT NULL ,
 `image` VARCHAR(255) NOT NULL ,
 PRIMARY KEY (`news_id`))
