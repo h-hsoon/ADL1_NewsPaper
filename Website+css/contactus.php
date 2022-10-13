12f46dbe515227c32e907aa39580f5afd25610c9
WaleedMuslem <waleedmosllm2020@gmail.com>
2022-10-13 23:30:03 +0200

    feat: creat contact page

---
+++ Website+css/contactus.php
@@ -0,0 +1,17 @@
+<?php require_once "header.php";?>
+
+<div class="box">
+<div class="about_us">
+
+<p>
+You can contact us by e-mail : NewsPaper.Eger@gmail.com 
+</p>
+<p>or on our platforms:</p>
+<p>
+    Facebook : NewsPaperEger@Facebook.com
+</p>
+<p>Twitter : NewsPaperEger@twitter.com</p>
+
+</div>
+</div>
+
