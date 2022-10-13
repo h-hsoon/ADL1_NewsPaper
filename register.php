2480fb7fba533eb9eca882ea0a2e4c1b69ac48e2
WaleedMuslem <waleedmosllm2020@gmail.com>
2022-10-13 23:31:14 +0200

    feat: creat register page

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php require_once "header.php";?>
+<div class="box">
+        
+        <h2>Register</h2>
+        
+        <form>
+            
+            <div class="inputBox">
+             <input type="" name="" required="">
+             <label>Username</label>
+            </div>
+            <div class="inputBox" >
+                <input type="password" name="" required="">
+                <label>Password</label>
+                
+            </div>
+            <div class="inputBox" >
+                <input type="password" name="" required="">
+                <label>Confirm Password</label>
+                
+            </div>
+            <input type="submit" name="" value="Submit">
+            <a href="##">I do have an account</a>
+        </form>
+    </div>
