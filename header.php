12aaa01f82b7a8fcd6a536f160a8835c539996a3
WaleedMuslem <waleedmosllm2020@gmail.com>
2022-10-13 23:33:24 +0200

    feat: creat header page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Longin</title>
+        <link rel="stylesheet" type="text/css" href="\test\css\style.css">
+    </head>
+<body>
+    <header>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">Home</a></li>    
+                <li><a href="#">Sign in</a></li> 
+                <li><a href="#">Register</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <form class="search-form">
+     
+                <input type="text" placeholder="search">
+                <button>search</button> 
+            </form>
+        </nav>
+    </header>
+    <main>
+
+
+    <!-- main content comes here-->
