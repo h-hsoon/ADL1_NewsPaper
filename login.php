31cd8caf7c3944ba64297e0daa37e57b15d39862
WaleedMuslem <waleedmosllm2020@gmail.com>
2022-10-13 23:32:11 +0200

    feat: creat login page

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php require_once "header.php";?>
+<div class="box">
+        
+        <h2>Login</h2>
+        
+        <form>
+            
+            <div class="inputBox">
+             <input type="" name="" required="">
+             <label>Username</label>
+            </div>
+            <div class="inputBox" >
+                <input type="password" name="" required="">
+                <label>Password</label>
+                
+            </div>
+            <input type="submit" name="" value="Submit">
+            <a href="##">I do not have an account</a>
+        </form>
+    </div>
