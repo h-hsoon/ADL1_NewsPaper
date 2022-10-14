a472a5f9dcd23e8f809d5c2196b433f041b90889
MaximusDD2002 <2maxim2@mail.ru>
2022-10-14 23:20:37 +0200

    Made an php file
    Made an php file for registering and accesing data from the database

---
+++ Website+css/functions.php
@@ -0,0 +1,76 @@
+<?php
+
+session_start();
+//initialising variables
+$username = '';
+$errors = array();
+
+//connect to database
+$db = new mysqli('localhost', 'root', '', 'adl1_newspaper') or die("couldn't connect");
+
+//register users
+if(isset($_POST['register'])){
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+//form validation 
+
+    if(empty($username)) array_push($errors, "Username is required");
+    if(empty($password_1)) array_push($errors, "Password is required");
+    if($password_1 != $password_2) array_push($errors, "Passwords do not match");
+
+//check db for the existing users with the same username
+
+    $users_check_query = "SELECT * FROM users WHERE user_name = '$username' LIMIT 1";
+    $results = mysqli_query($db, $users_check_query);
+    $users = $mysqli_fetch_assoc($result);
+
+    if($users){
+       if($users['username'] === $username) array_push($errors, "Username already exists");
+    }
+
+//Reggisters the user
+    if(count($errors) == 0){
+        $password = md5($password_1); //this will encrypt the password
+        $query = "INSERT INTO users (user_name, password) VALUES ('$username', '$password')";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You have been registered, mortal";
+
+        echo "sus";
+
+        header('location: homepage.php');
+    }
+}
+
+//Login users
+
+if(isset($_POST['login_users'])){
+$username = mysqli_real_escape_string($db, $_POST['username']);
+$password = mysqli_real_escape_string($db, $_POST['password']);
+
+if(empty($username)){
+    array_push($errors, "Username is required");
+}
+
+if(empty($password)){
+    array_push($errors, "Password is required");
+}
+
+if(count($errors) == 0){
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE user_name = '$username' AND password = '$password' ";
+    $results = mysqli_query($db, $query);
+
+    if (mysqli_num_rows($results)){
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: homepage.php');
+    } else {
+        array_push($errors, "Wrong username or password. Try again");
+    }
+}
+}
+
+?>
