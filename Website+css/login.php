d694029995314bdc2fb5bb2875d799da73eec910
MaximusDD2002 <2maxim2@mail.ru>
2022-10-14 23:17:58 +0200

    Made some adjustments
    Made some adjustments in the login php file

--- Website+css/login.php
+++ Website+css/login.php
@@ -1,16 +1,17 @@
+<?php include('functions.php') ?>
 <?php require_once "header.php";?>
 <div class="box">
         
         <h2>Login</h2>
         
-        <form>
+        <form action = "login.php" method="post">
             
             <div class="inputBox">
-             <input type="" name="" required="">
+             <input type="username" name="username" required>
              <label>Username</label>
             </div>
             <div class="inputBox" >
-                <input type="password" name="" required="">
+                <input type="password" name="password_1" required>
                 <label>Password</label>
                 
             </div>
