f6f64d863219a7a032f6bccf1e4109f6e8a889cf
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-10-18 20:33:44 +0200

    feat<header.php>: add bootstrap library calling

--- Website+css/header.php
+++ Website+css/header.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Longin</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
+
+<head>
+    <title>Longin</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--jQuery and Bootstrap Bundle (includes Popper [for tooltip]) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
 <body>
     <header>
         <nav class="menu">
             <ul>
-                <li><a href="homepage.php">Home</a></li>    
-                <li><a href="login.php">Sign in</a></li> 
+                <li><a href="homepage.php">Home</a></li>
+                <li><a href="login.php">Sign in</a></li>
                 <li><a href="register.php">Register</a></li>
                 <li><a href="aboutus.php">About</a></li>
                 <li><a href="contactus.php">Contact</a></li>
             </ul>
             <form class="search-form">
-     
+
                 <input type="text" placeholder="search">
-                <button>search</button> 
+                <button>search</button>
             </form>
         </nav>
     </header>
     <main>
 
 
-    <!-- main content comes here-->
+        <!-- main content comes here-->
